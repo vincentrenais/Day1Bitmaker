5d709bffdfba8913a087c04493009336cf146bdb
Vincent Renais <vincentrenais@gmail.com>
2015-04-06 15:50:23 -0400

    Social Features

---
+++ BitmakerLabsDay1/Feature.h
@@ -0,0 +1,13 @@
+//
+//  Feature.h
+//  BitmakerLabsDay1
+//
+//  Created by Vincent Renais on 2015-04-06.
+//  Copyright (c) 2015 Vincent Renais. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Feature : NSObject
+
+@end
---
+++ BitmakerLabsDay1/Feature.m
@@ -0,0 +1,13 @@
+//
+//  Feature.m
+//  BitmakerLabsDay1
+//
+//  Created by Vincent Renais on 2015-04-06.
+//  Copyright (c) 2015 Vincent Renais. All rights reserved.
+//
+
+#import "Feature.h"
+
+@implementation Feature
+
+@end
