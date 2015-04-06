25364ff8c26193a97fdd6f9b14c382689810e8b4
Vincent Renais <vincentrenais@gmail.com>
2015-04-06 15:40:25 -0400

    This is a second commit

--- BitmakerLabsDay1/ViewController.m
+++ BitmakerLabsDay1/ViewController.m
@@ -20,7 +20,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+//This is a second comment
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
